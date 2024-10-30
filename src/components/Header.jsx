d5700b02e43d30d75c76c5e9731b42a89365610a
Ayomi <layomilawore@gmail.com>
2024-10-30 17:07:08 +0100

    orders page implemented. Pagination feature included

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({category, title}) => {
   return (
-    <div>Header</div>
+    <div className='mt-10'>
+      <p className='text-gray-300'>
+        {category}
+      </p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-500'>{title}</p>
+    </div>
   )
 }
 
