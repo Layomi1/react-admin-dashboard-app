4416aa980643cb6a4d3e06b22683833cfb1d20ca
Ayomi <layomilawore@gmail.com>
2024-11-01 10:40:13 +0100

    dditor app implemnted

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {Header}from '../components';
+import {HtmlEditor, Image, Toolbar, Link, QuickToolbar, Inject,RichTextEditorComponent} from "@syncfusion/ej2-react-richtexteditor"
+import {EditorData} from "../data/dummy"
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent >
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]}/>
+      </RichTextEditorComponent>
+      </div>
+
   )
 }
 
