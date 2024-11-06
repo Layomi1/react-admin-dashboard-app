f949fed7f4c69f72fd1dad9a0cc5f9830e977f39
Ayomi <layomilawore@gmail.com>
2024-11-06 10:07:01 +0100

    pyramid chart page implemnted

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import {
+  AccumulationSeriesCollectionDirective,
+   AccumulationSeriesDirective,
+   AccumulationChartComponent,
+   AccumulationLegend, 
+   PyramidSeries,AccumulationDataLabel,
+   AccumulationTooltip,
+   Inject,
+} from "@syncfusion/ej2-react-charts";
+
+
+import { useStateContext } from "../../contexts/contextProvider";
+import { PyramidData } from "../../data/dummy";
 
 const Pyramid = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Pyramid</div>
+    <AccumulationChartComponent
+    id='charts'
+    // legendSettings={{
+    //   visible: legendVisibity,
+    //   background:'fff',
+      
+    // }}
+    height='420px'
+    tooltip={{enable:true}}
+    background={currentMode === "Dark" ? "#33373E" : "#fff"}
+  >
+    <Inject services={[AccumulationLegend,
+      PyramidSeries, AccumulationDataLabel,AccumulationTooltip
+    ]} />
+    <AccumulationSeriesCollectionDirective>
+      <AccumulationSeriesDirective 
+      name="Diets"
+      dataSource={PyramidData}
+      xName="x"
+      yName="y"
+     
+      innerRadius="40%"
+      startAngle={0}
+      endAngle={360}
+      radius="70%"
+      explodeIndex={2}
+      dataLabel={{
+        visible:true,
+        name:'text',
+        position: 'Inside',
+        font:{
+          fontWeight:'600',
+          color: '#fff'
+        }
+      }}
+      type="Pyramid"
+      />
+    </AccumulationSeriesCollectionDirective>
+  </AccumulationChartComponent>
   )
 }
 
-export default Pyramid
+export default Pyramid 
