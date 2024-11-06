e14f0c3038a318f3d41570b63df1e983cdbc6551
Ayomi <layomilawore@gmail.com>
2024-11-06 10:16:45 +0100

    stacked page implemented

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import {Header, Stacked as StackedChart} from "../../components"
 
 const Stacked = () => {
   return (
-    <div>Stacked</div>
-  )
-}
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="chart" title="Stack Chart" />
+      <div className="w-full mt-10  mx-auto">
+        <StackedChart width='100%' height='420px'/>
+      </div>
+    </div>
+  );
+};
 
-export default Stacked
+export default Stacked;
