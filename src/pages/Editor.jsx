c7c713123d1a9cbd96f2efd48b1850ef3ab3dfae
Ayomi <layomilawore@gmail.com>
2024-11-08 11:13:31 +0100

    editor page issue has been fixed

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,14 +1,16 @@
 import {Header}from '../components';
 import {HtmlEditor, Image, Toolbar, Link, QuickToolbar, Inject,RichTextEditorComponent} from "@syncfusion/ej2-react-richtexteditor"
-import {EditorData} from "../data/dummy"
 
 const Editor = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Editor" />
-      <RichTextEditorComponent >
+      <div className='mx-auto'>
+      <RichTextEditorComponent style={{zIndex: 0}}>
         <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]}/>
       </RichTextEditorComponent>
+      </div>
+     
       </div>
 
   )
